b3bdbc65fceb23728b88173cf5d4784260a5a5c6
Yann Kamche <yannkamche@gmail.com>
2024-05-25 13:35:48 +0100

    completed the sidebar and added shadcn button

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,14 @@
+import Sidebar from '@/components/shared/Sidebar';
 import React from 'react'
 
 // Layouts always export some children within them and allows you to put shared UI
 const Layout = ({ children }: { children: React.ReactNode}) => {
   return (
     <main className="root">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Mobile */}
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
