d138373d627c2c3581ae8742f25fc0b9ffa39d29
Yann Kamche <yannkamche@gmail.com>
2024-06-01 07:56:17 +0100

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up"]);
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+  "/sign-in",
+  "/sign-up",
+  "/api/webhooks/clerk",
+]);
 
 export default clerkMiddleware((auth, request) => {
+  // Check if the request matches any public routes
   if (!isPublicRoute(request)) {
+    // Protect all non-public routes
     auth().protect();
   }
 });
